7d6b21da7d0201c6e0df09d59e66763b8b3d58a5
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2024-03-06 16:10:45 +0300

    Run Backend and fronted as the same time

--- client/src/Api/axios.js
+++ client/src/Api/axios.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   //local server
-  // baseURL: "http://localhost:5000/",
+  baseURL: "http://localhost:5000/",
   //deployed version of amazon server on cyclic.com
-  baseURL: "https://easy-sneakers-calf.cyclic.app/",
+  // baseURL: "https://easy-sneakers-calf.cyclic.app/",
 });
 
 export { axiosInstance };
